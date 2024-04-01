b765e1bc18a93797853288150c6403ec34724a88
Vladimir Tsymbaliuk <89538231+RichardDitrix@users.noreply.github.com>
2024-04-01 20:56:34 +0300

    1

--- index.js
+++ index.js
@@ -18,21 +18,16 @@ async function invokeAction({ action, id, name, email, phone }) {
         break;
       case "get":
         const contact = await contacts.getContactById(id);
-        if (!contact) throw new Error(`null`);
         console.log(contact);
         break;
       case "add":
         const newContact = await contacts.addContact(name, email, phone);
-        if (!newContact)
-          throw new Error(
-            `Contact with name=${name}, email=${email}, phone=${phone} not added`
-          );
+     
         console.log(newContact);
         break;
       case "remove":
         const removedContact = await contacts.removeContact(id);
-        if (!removedContact)
-          throw new Error(`null`);
+       
         console.log(removedContact);
         break;
       default:
